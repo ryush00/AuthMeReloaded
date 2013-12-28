a3fde6f3a29056ffd6b04396d15edd0e4c740978
iBo3oF <iBo3oF@users.noreply.github.com>
2013-12-28 15:09:13 +0200

    Update RegisterCommand.java
    Add LoginEvent firing after players is registered & logged in, for scoreboard plugins.

--- src/fr/xephi/authme/commands/RegisterCommand.java
+++ src/fr/xephi/authme/commands/RegisterCommand.java
@@ -31,6 +31,7 @@ import fr.xephi.authme.settings.PlayersLogs;
 import fr.xephi.authme.settings.Settings;
 import fr.xephi.authme.task.MessageTask;
 import fr.xephi.authme.task.TimeoutTask;
+import fr.xephi.authme.events.LoginEvent;
 
 
 public class RegisterCommand implements CommandExecutor {
@@ -262,6 +263,8 @@ public class RegisterCommand implements CommandExecutor {
                 if(plugin.notifications != null) {
                 	plugin.notifications.showNotification(new Notification("[AuthMe] " + player.getName() + " has registered!"));
                 }
+                // The Loginevent now fires (as intended) after everything is processed
+                Bukkit.getServer().getPluginManager().callEvent(new LoginEvent(player, true));
             } catch (NoSuchAlgorithmException ex) {
                 ConsoleLogger.showError(ex.getMessage());
                 sender.sendMessage(m._("error"));
